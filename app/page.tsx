ae4ed16d5892c794931cb6a63290a40334bf7ba4
Théo <theo.rgr2404@gmail.com>
2026-07-09 14:48:48 +0200

    add footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import SideActions from "@/components/SideActions";
 import StudyPrograms from "@/components/StudyPrograms/StudyPrograms";
+import Footer from "@/components/Footer";
 
 export default function Home() {
     return (
@@ -10,6 +11,7 @@ export default function Home() {
             <Hero />
             <SideActions />
             <StudyPrograms />
+            <Footer />
         </main>
     );
 }
