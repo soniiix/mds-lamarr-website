6bdad3642972ca1aa90270316e58ea5c26d664f7
soniiix <quent.couzinet@gmail.com>
2026-07-09 11:07:38 +0200

    feat: add SideActions component to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import SideActions from "@/components/SideActions";
 
 export default function Home() {
     return (
-        <main>
+        <main className="h-400">
             <Header />
             <Hero />
+            <SideActions />
         </main>
     );
 }
